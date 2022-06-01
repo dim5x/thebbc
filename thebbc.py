567b4af9f2006de4f9878ac0c0e979d7730ab923
dim5x <49735406+dim5x@users.noreply.github.com>
2022-06-01 05:12:33 +0300

    Update thebbc.py

--- thebbc.py
+++ thebbc.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'hello'
+    res = requests.get('https://ria.ru/authors/')
+    return 'hello' + res.status_code
 
 if __name__=="__main__":
     app.run(debug=True)
