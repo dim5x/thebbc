094416d4e07f852586199508e9312569179dfb6c
dim5x <dim5x@yahoo.com>
2022-06-02 01:58:22 +0300

    1

--- thebbc.py
+++ thebbc.py
@@ -1,25 +1,38 @@
 from flask import Flask
 import requests
+import os
+import yadisk
+
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def hello():
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/41.0.2228.0 Safari/537.3'}
+                                 'Chrome/41.0.2228.0 Safari/537.3'}
         res1 = requests.get('https://github.com/', headers=headers)
         res2 = requests.get('https://ria.ru/', headers=headers)
         res3 = requests.get('https://google.com', headers=headers)
-        
+
+        try:
+            y = yadisk.YaDisk(token=os.environ['token'])
+            y.upload("requirements.txt", "/test-dir/requirements.txt")
+        except:
+            return 'Ya error'
+
         return f'''hello
         Github = {str(res1.status_code)} 
         Ria = {str(res2.status_code)}
         Google = {str(res3.status_code)}
+        Yandex = ok.
         '''
+
     except:
         return 'Error'
 
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
