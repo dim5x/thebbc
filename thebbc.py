2b887593231c9e464f5ed35c9d85a84481ac09b6
dim5x <49735406+dim5x@users.noreply.github.com>
2022-06-01 05:46:14 +0300

    Update thebbc.py

--- thebbc.py
+++ thebbc.py
@@ -6,10 +6,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello():
     try:
-        res1 = requests.get('https://github.com/')
-        res2 = requests.get('https://ria.ru/')
-        res3 = requests.get('https://google.com')
-        return 'hello' + str(res1.status_code) + str(res2.status_code) + str(res3.status_code)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/41.0.2228.0 Safari/537.3'}
+        res1 = requests.get('https://github.com/', headers=headers)
+        res2 = requests.get('https://ria.ru/', headers=headers)
+        res3 = requests.get('https://google.com', headers=headers)
+        
+        return f'''hello
+        Github = {str(res1.status_code)} 
+        Ria = {str(res2.status_code)}
+        Google = {str(res3.status_code)}
+        '''
     except:
         return 'Error'
 
